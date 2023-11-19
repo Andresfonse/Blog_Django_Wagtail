4e798b0e1d5ab6392e647b796d5de72fbabaf087
deavila747 <cristiandeavila747@gmail.com>
2023-11-19 04:01:34 -0500

    Blo-dJango-with Styles

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@)^184_5ad0s2r5vw&zhu5vv12$2rj74_sphstz0tor%n###h8"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
